95499c9914724c73dc4117166582d13fc19dff34
Gabriel Malpica <gabrielmalpicacruz@gmail.com>
2025-04-22 20:32:47 -0500

    Se arregló para que la fecha este con la de Colombia

--- scripts/utils.ts
+++ scripts/utils.ts
@@ -100,13 +100,14 @@ export const arregloNumeros = [
     { conjunto: "Torres de San Juan", nombre: "ROCIO", numero: "573144603942", rol: "ADMINISTRADOR" },
 ];
 
-const currentDate = new Date();
+const currentDate = new Date(new Date().toLocaleString('en-US', { timeZone: 'America/Bogota' }));
 const year = currentDate.getFullYear();
 const month = String(currentDate.getMonth() + 1).padStart(2, '0');
 const day = String(currentDate.getDate()).padStart(2, '0');
 const hours = String(currentDate.getHours()).padStart(2, '0');
 const minutes = String(currentDate.getMinutes()).padStart(2, '0');
 const seconds = String(currentDate.getSeconds()).padStart(2, '0');
+
 export const formattedTime = `${day}-${month}-${year} ${hours}:${minutes}:${seconds}`;
 
 // Verificar si el bot global está activo
