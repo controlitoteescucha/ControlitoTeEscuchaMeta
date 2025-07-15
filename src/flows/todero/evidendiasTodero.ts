f02756f7adae28fc4e15ba4ece6442861da968b4
Gabriel Malpica <gabrielmalpicacruz@gmail.com>
2025-07-15 15:44:23 -0500

    Se arreglo error de flujo evidenciasTodero solo se estaba subiendo la ultima imagen

--- src/flows/todero/evidendiasTodero.ts
+++ src/flows/todero/evidendiasTodero.ts
@@ -10,6 +10,20 @@ const spreadsheetId = '1YWx_MhJ8mSxZiA6RaZv91sX965KMBxtvenN9FdXWdL0';
 const driveId = '1PFuyYI-S1huUX75eMyCLPb9EUZ1Tf2bC';
 
 export const evidenciasTodero = addKeyword('3')
+    .addAction(
+        async(ctx, ctxFn) => {
+            const userInfo = ctxFn.state.getMyState();
+            const fecha = getFormattedTime();
+            await appendToSheet([ 
+                [
+                    fecha, 
+                    userInfo.conjunto, 
+                    ctx.from, 
+                    userInfo.nombreCompleto,
+                ]
+            ], spreadsheetId , userInfo.conjunto);
+        }
+    )
     .addAnswer("📸 *¡Necesitamos una foto como evidencia!*\n\nPor favor, envíame una imagen de como estaba antes de realizar la actividad. 😊", 
         { capture: true }, 
         async (ctx, ctxFn) => {
@@ -33,6 +47,26 @@ export const evidenciasTodero = addKeyword('3')
             } catch (error) {
                 console.error('Error al guardar archivo:', error);
                 return ctxFn.fallBack("❌ Ocurrió un error al procesar tu imagen. Por favor, inténtalo nuevamente.");
+            }
+
+            try {
+                const currentState = ctxFn.state.getMyState();
+                await uploadFileLegacy(
+                    currentState.fotoAntes, 
+                    `${ctx.from}-${ctx.pushName}`, 
+                    currentState.conjunto, 
+                    driveId, 
+                    spreadsheetId, 
+                    currentState.mimeType
+                );
+            } catch (error) {
+                console.error('Error al subir archivo (antes):', error);
+                return ctxFn.fallBack("❌ Ocurrió un error al subir tu imagen 'antes'. Por favor, inténtalo nuevamente.");
+            } finally {
+                const currentState = ctxFn.state.getMyState();
+                if (fs.existsSync(currentState.fotoAntes)) {
+                    fs.unlinkSync(currentState.fotoAntes);
+                }
             }
     
         }
@@ -61,41 +95,7 @@ export const evidenciasTodero = addKeyword('3')
                 console.error('Error al guardar archivo:', error);
                 return ctxFn.fallBack("❌ Ocurrió un error al procesar tu imagen. Por favor, inténtalo nuevamente.");
             }
-    
-        }
-    )
-    .addAnswer("✅ *¡Gracias por tu colaboración!*\n\nEl registro de la actividad ha sido exitoso. 😊\n\n¡Tu trabajo es muy valioso para nosotros! 🌟", 
-        null, 
-        async (ctx, ctxFn) => {
-            const userInfo = ctxFn.state.getMyState();
-            const fecha = getFormattedTime();
-            await appendToSheet([ 
-                [
-                    fecha, 
-                    userInfo.conjunto, 
-                    ctx.from, 
-                    userInfo.nombreCompleto,
-                ]
-            ], spreadsheetId , userInfo.conjunto);
-            try {
-                const currentState = ctxFn.state.getMyState();
-                await uploadFileLegacy(
-                    currentState.fotoAntes, 
-                    `${ctx.from}-${ctx.pushName}`, 
-                    currentState.conjunto, 
-                    driveId, 
-                    spreadsheetId, 
-                    currentState.mimeType
-                );
-            } catch (error) {
-                console.error('Error al subir archivo (antes):', error);
-                return ctxFn.fallBack("❌ Ocurrió un error al subir tu imagen 'antes'. Por favor, inténtalo nuevamente.");
-            } finally {
-                const currentState = ctxFn.state.getMyState();
-                if (fs.existsSync(currentState.fotoAntes)) {
-                    fs.unlinkSync(currentState.fotoAntes);
-                }
-            }
+
             try {
                 const currentState = ctxFn.state.getMyState();
                 await uploadFileLegacy(
@@ -115,6 +115,12 @@ export const evidenciasTodero = addKeyword('3')
                     fs.unlinkSync(currentState.fotoDespues);
                 }
             }
+    
+        }
+    )
+    .addAnswer("✅ *¡Gracias por tu colaboración!*\n\nEl registro de la actividad ha sido exitoso. 😊\n\n¡Tu trabajo es muy valioso para nosotros! 🌟", 
+        null, 
+        async (ctxFn) => {
             ctxFn.endFlow();
         }
     );
